2f926b739dacc8e158878c4c3d7e0dfab4091338
mvicariaf <amivicaria@hotmail.es>
2017-09-28 10:05:30 +0200

    first commit

---
+++ src/packman/Test1.java
@@ -0,0 +1,12 @@
+package packman;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Laberinto laberinto1;
+		laberinto1 = new Laberinto();
+		laberinto1.imprimirLaberinto();
+	}
+
+}
